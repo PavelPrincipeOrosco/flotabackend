8ec18d52891b95311a00875a85ea2b1541338375
PAVEL PRINCIPE <pprincipe.cons@gmail.com>
2021-07-27 18:55:51 -0500

    mod inicial

---
+++ src/main/java/com/incloud/hcp/jco/maestro/service/impl/JCOProveedorServiceImpl.java
@@ -0,0 +1,30 @@
+package com.incloud.hcp.jco.maestro.service.impl;
+
+import com.incloud.hcp.jco.maestro.dto.FiltroProveedorDto;
+import com.incloud.hcp.jco.maestro.dto.ProveedorDto;
+import com.incloud.hcp.jco.maestro.service.JCOProveedorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = Exception.class)
+public class JCOProveedorServiceImpl implements JCOProveedorService {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public List<ProveedorDto> listaProveedor(FiltroProveedorDto dto) throws Exception {
+        logger.error("listaProveedor__" + dto);
+        List<ProveedorDto> lista = new ArrayList<ProveedorDto>();
+        ProveedorDto dtoUno = new ProveedorDto();
+        dtoUno.setRazonSocial("rz 01");
+        dtoUno.setRuc("10987666543");
+        lista.add(dtoUno);
+        return lista;
+    }
+
+}
